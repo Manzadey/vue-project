20818ea636a50f99a76ef270d4ae3b0a0ed07b71
Andrey Manzadey <andrey.manzadey@gmail.com>
2023-07-07 19:52:54 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ import ru from 'dayjs/locale/ru';
 import {createApp} from 'vue'
 import App from './App.vue'
 import components from '@/components/UI';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 const vm = createApp(App)
 
 components.forEach(component => vm.component(component.name, component));
 
 dayjs.locale('ru', ru);
+dayjs.extend(relativeTime)
 
 vm.config.globalProperties.$dayjs = dayjs;
 
